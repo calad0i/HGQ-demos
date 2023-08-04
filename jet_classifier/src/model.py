504e6a0db3e139523186664fec53dbc58572e6f2
Chang Sun <chsun@cern.ch>
2023-08-04 16:58:59 +0200

    fix: wrong argument in jet model def

--- jet_classifier/src/model.py
+++ jet_classifier/src/model.py
@@ -19,7 +19,6 @@ def get_model(bops_reg_factor, a_bw_l1_reg=0., w_bw_l1_reg=0., a_init_bw=2, w_in
         regularizer=L1(w_bw_l1_reg),
     )
 
-
     act_q_conf = dict(
         init_bw=a_init_bw,
         skip_dims=(0,),  # same as 'batch'
@@ -28,13 +27,12 @@ def get_model(bops_reg_factor, a_bw_l1_reg=0., w_bw_l1_reg=0., a_init_bw=2, w_in
         dtype=None,
         bw_clip=(-23, 23),
         trainable=True,
-        regularizer=L1(2e-6),
-        minmax_record=a_bw_l1_reg
+        regularizer=L1(a_bw_l1_reg),
+        minmax_record=True
     )
 
     set_default_kernel_quantizer_config(ker_q_conf)
     set_default_pre_activation_quantizer_config(act_q_conf)
-
 
     model = keras.Sequential([
         HQuantize(input_shape=(16,), name='inp_q', bops_reg_factor=bops_reg_factor),
